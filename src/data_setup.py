I have finished the first model, including accuracy, precision, recall and F1_score, but the rest of models in this project I haven't corrected.

src/data_setup.py
@@ -3,7 +3,7 @@ import pandas as pd
 import torchaudio
 import torch
 from torchvision import transforms
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
 from utils import  MonoToColor
 
 
@@ -66,14 +66,16 @@ if __name__ == "__main__":
     SAMPLE_RATE = 22050 
     NUM_SAMPLES = 22050
     MEL_SPECTROGRAM = torchaudio.transforms.MelSpectrogram(
-        sample_rate=SAMPLE_RATE, 
-        n_fft=1024, 
+        sample_rate=SAMPLE_RATE,
+        n_fft=1024,
         hop_length=512, 
-        n_mels=64
-    )
-    
-    drone_audio = DroneAudioDataset(ANNOTATIONS_FILE, AUDIO_DIR, SAMPLE_RATE, NUM_SAMPLES, MEL_SPECTROGRAM)
+        n_mels=64)
+    TRANSFORMATION = transforms.Compose([MEL_SPECTROGRAM, MonoToColor()])
+    drone_audio = DroneAudioDataset(ANNOTATIONS_FILE, AUDIO_DIR, SAMPLE_RATE, NUM_SAMPLES, TRANSFORMATION)
     print(f"There are {len(drone_audio)} samples in the dataset.")
     signal, label = drone_audio[1]
     print(signal)
     print(signal.shape)
+    
+    data_iter = DataLoader(dataset=drone_audio, batch_size=64, shuffle=True)
+    print(next(iter(data_iter)))

src/engine.py
@@ -1,7 +1,6 @@
 import torch
 
 
-
 def train_step(model, dataloader, loss_fn, optimizer, device, is_RNN=False):
     train_loss, train_acc = 0, 0
     model.train()
@@ -23,27 +22,27 @@ def train_step(model, dataloader, loss_fn, optimizer, device, is_RNN=False):
 
 
 
-def test_step(model, dataloader, loss_fn, device, is_RNN=False):
+def val_step(model, dataloader, loss_fn, device, is_RNN=False):
     model.eval()
-    test_loss, test_acc = 0, 0
+    val_loss, val_acc = 0, 0
     with torch.inference_mode():
-        for X_test, y_test in dataloader:
+        for X_val, y_val in dataloader:
             if is_RNN is True:
-                X_test, y_test = X_test.reshape(-1, 44, 64).to(device), y_test.to(device)
+                X_val, y_val = X_val.reshape(-1, 44, 64).to(device), y_val.to(device)
             else:
-                X_test, y_test = X_test.to(device), y_test.to(device)
-            test_pred = model(X_test)
-            test_loss += loss_fn(test_pred, y_test).item()
-            test_acc += torch.eq(test_pred.argmax(dim=1), y_test).sum().item() / len(test_pred)
-        test_loss /= len(dataloader)
-        test_acc /= len(dataloader)
-    return test_loss, test_acc
+                X_val, y_val = X_val.to(device), y_val.to(device)
+            val_pred = model(X_val)
+            val_loss += loss_fn(val_pred, y_val).item()
+            val_acc += torch.eq(val_pred.argmax(dim=1), y_val).sum().item() / len(val_pred)
+        val_loss /= len(dataloader)
+        val_acc /= len(dataloader)
+    return val_loss, val_acc
 
 
 
-def train(model, train_dataloader, test_dataloader, loss_fn, optimizer, scheduler, epochs, device, is_RNN):
+def train(model, train_dataloader, val_dataloader, loss_fn, optimizer, scheduler, epochs, device, is_RNN):
     
-    results = {"train_loss": [], "train_acc": [], "test_loss": [], "test_acc": []}
+    results = {"train_loss": [], "train_acc": [], "val_loss": [], "val_acc": []}
     for epoch in range(epochs):
         train_loss, train_acc = train_step(
             model=model, 
@@ -53,17 +52,17 @@ def train(model, train_dataloader, test_dataloader, loss_fn, optimizer, schedule
             device=device, 
             is_RNN=is_RNN)
         scheduler.step()
-        test_loss, test_acc = test_step(
+        val_loss, val_acc = val_step(
             model=model, 
-            dataloader=test_dataloader, 
+            dataloader=val_dataloader, 
             loss_fn=loss_fn, 
             device=device, 
             is_RNN=is_RNN) 
         print(f"Epoch: {epoch + 1} | "
               f"train_loss: {train_loss: .4f} | train_acc: {train_acc:.4f} | "
-              f"test_loss: {test_loss:.4f} | test_acc: {test_acc:.4f}")
+              f"val_loss: {val_loss:.4f} | val_acc: {val_acc:.4f}")
         results["train_loss"].append(train_loss)
         results["train_acc"].append(train_acc)
-        results["test_loss"].append(test_loss)
-        results["test_acc"].append(test_acc) 
+        results["val_loss"].append(val_loss)
+        results["val_acc"].append(val_acc) 
     return results

src/model_builder.py
@@ -41,32 +41,15 @@ class CustomCNN(nn.Module):
         return predictions
 
 
-
-class Resnet18(nn.Module):
+class ResNet18(nn.Module):
     
-    def __init__(self, num_classes=5, dropout_prob=0.2):
-        super(Resnet18, self).__init__()
-        self.resnet = resnet18(weights=None)
-        self.resnet.fc = nn.Linear(512, num_classes)
-        self.dropout = nn.Dropout(dropout_prob)
-        self.flatten = nn.Flatten()
+    def __init__(self, num_classes):
+        super(ResNet18, self).__init__()
+        self.resnet_18 = resnet18(weights=None)
+        self.resnet_18.fc = nn.Linear(512, num_classes)
         
     def forward(self, x):
-        x = self.resnet.conv1(x)
-        x = self.resnet.bn1(x)
-        x = self.resnet.relu(x)
-        x = self.resnet.maxpool(x)
-
-        x = self.resnet.layer1(x)
-        x = self.resnet.layer2(x)
-        x = self.resnet.layer3(x)
-        x = self.resnet.layer4(x)
-
-        x = self.resnet.avgpool(x)
-        x = self.flatten(x)
-
-        x = self.dropout(x)
-        x = self.resnet.fc(x)
+        x = self.resnet_18(x)
         return x
 
 
@@ -123,3 +106,11 @@ class LSTM(nn.Module):
         x = x[:, -1, :]
         x = self.fc(x)
         return x 
+    
+    
+if __name__ == "__main__":
+    a = torch.randn(32, 3, 64, 44)
+    model = ResNet18(num_classes=6)
+    y = model(a)
+    print(y)
+    print(y.shape)

src/train_model_1_ResNet18.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 from torchaudio.transforms import MelSpectrogram, AmplitudeToDB
 from torch.utils.data import DataLoader
 from timeit import default_timer as timer
-from model_builder import Resnet18
+from model_builder import ResNet18
 from data_setup import DroneAudioDataset
 from utils import split_dataset, plot_loss_curves, plot_acc_curves, MonoToColor, save_model
 from engine import train
@@ -21,12 +21,11 @@ SAMPLE_RATE = 22050
 NUM_SAMPLES = 22050
 TRANSFORMATION_1 = transforms.Compose(
     [MelSpectrogram(sample_rate=SAMPLE_RATE, n_fft=1024, hop_length=512, n_mels=64), 
-     AmplitudeToDB(stype="power", top_db=80), 
      MonoToColor()])
 
 
 
-model_1 = Resnet18().to(DEVICE)
+model_1 = ResNet18(num_classes=6).to(DEVICE)
 criterion = nn.CrossEntropyLoss()
 trainer = torch.optim.Adam(model_1.parameters(), lr=LEARNING_RATE)
 Drone_audio = DroneAudioDataset(ANNOTATIONS_FILE, AUDIO_DIR, SAMPLE_RATE, NUM_SAMPLES, TRANSFORMATION_1)
@@ -34,10 +33,13 @@ adjuster = torch.optim.lr_scheduler.StepLR(optimizer=trainer, step_size=7, gamma
 
 
 
-train_dataset, test_dataset = split_dataset(dataset=Drone_audio, train_radio=0.8, random_seed=None)
+Drone_audio = DroneAudioDataset(ANNOTATIONS_FILE, AUDIO_DIR, SAMPLE_RATE, NUM_SAMPLES, TRANSFORMATION_1)
+train_dataset, val_dataset, test_dataset = split_dataset(Drone_audio, train_radio=0.8, test_ratio=0.1, random_seed=42)
 train_iter = DataLoader(dataset=train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+val_iter = DataLoader(dataset=val_dataset, batch_size=BATCH_SIZE, shuffle=True)
 test_iter = DataLoader(dataset=test_dataset, batch_size=BATCH_SIZE, shuffle=False)
 print("Dataset Ready!")
+print("-" * 60)
 
 
 
@@ -54,7 +56,8 @@ model_results = train(
     is_RNN=False
 )
 end_time = timer()
-print(f"Total training time: {end_time - start_time:.3f} seconds")
+total_time_model_1 = end_time - start_time
+print(f"Total training time: {total_time_model_1:.3f} seconds")
 save_model(model=model_1, 
            target_dir=r"C:\MachineLearning\Graduation_Project\models", 
            model_name="Model_1_ResNet18.pth")

src/train_model_4_LSTM.py
@@ -9,21 +9,21 @@ from engine import train
 from utils import split_dataset, plot_loss_curves, plot_acc_curves, save_model
 
 
-
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 INPUT_SIZE = 64
 SQUENCE_LENGTH = 44
 NUM_LAYERS = 2
 HIDDEN_SIZE = 256
-NUM_CLASSES = 5
+NUM_CLASSES = 6
 BATCH_SIZE = 64
 EPOCHS = 20
 LEARNING_RATE = 0.001
+
+
 model_4 = LSTM(INPUT_SIZE, HIDDEN_SIZE, NUM_LAYERS, NUM_CLASSES, DEVICE).to(DEVICE)
 criterion = nn.CrossEntropyLoss()
 trainer = torch.optim.Adam(model_4.parameters(), lr=LEARNING_RATE)
 adjuster = torch.optim.lr_scheduler.StepLR(optimizer=trainer, step_size=7, gamma=0.1)
-
 
 
 ANNOTATIONS_FILE = r"C:\MachineLearning\Graduation_Project\data\DroneAudio.xlsx"
@@ -31,12 +31,15 @@ AUDIO_DIR = r"C:\MachineLearning\Graduation_Project\data\DroneAudio_Mono_16K"
 SAMPLE_RATE = 22050
 NUM_SAMPLES = 22050
 TRANSFORMATION_0 = MelSpectrogram(sample_rate=SAMPLE_RATE, n_fft=1024, hop_length=512, n_mels=64)
+
+
 Drone_audio = DroneAudioDataset(ANNOTATIONS_FILE, AUDIO_DIR, SAMPLE_RATE, NUM_SAMPLES, TRANSFORMATION_0)
-train_dataset, test_dataset = split_dataset(dataset=Drone_audio, train_radio=0.8, random_seed=None)
+train_dataset, val_dataset, test_dataset = split_dataset(Drone_audio, train_radio=0.8, test_ratio=0.1, random_seed=42)
 train_iter = DataLoader(dataset=train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+val_iter = DataLoader(dataset=val_dataset, batch_size=BATCH_SIZE, shuffle=True)
 test_iter = DataLoader(dataset=test_dataset, batch_size=BATCH_SIZE, shuffle=False)
 print("Dataset Ready!")
-print("-----------------------------------------------------------------------------------------------------")
+print("-" * 60)
 
 
 
@@ -53,7 +56,8 @@ model_results = train(
     is_RNN=True
 )
 end_time = timer()
-print(f"Total training time: {end_time - start_time:.3f} seconds")
+total_time_model_4 = end_time - start_time
+print(f"Total training time: {total_time_model_4:.3f} seconds")
 save_model(model=model_4, 
            target_dir=r"C:\MachineLearning\Graduation_Project\models", 
            model_name="Model_4_LSTM.pth")

src/utils.py
@@ -120,13 +120,16 @@ class MonoToColor(nn.Module):
     
     
     
-def split_dataset(dataset, train_radio, random_seed=None):
+def split_dataset(dataset, train_radio, test_ratio, random_seed=None):
     if random_seed is not None:
         torch.manual_seed(random_seed)
-    train_size = int(train_radio * len(dataset))
-    test_size = len(dataset) - train_size
-    train_data, test_data = random_split(dataset=dataset, lengths=[train_size, test_size])
-    return train_data, test_data
+    test_size = int(test_ratio * len(dataset))
+    train_val_size = len(dataset) - test_size
+    train_val_data, test_data = random_split(dataset, [train_val_size, test_size])
+    train_size = int(train_radio * len(train_val_data))
+    val_size = train_val_size - train_size
+    train_data, val_data = random_split(train_val_data, [train_size, val_size]) 
+    return train_data, val_data, test_data
 
 
 
@@ -137,17 +140,18 @@ def plot_loss_curves(training_results: dict[str, list[float]]):
     Args:
         training_results (dict): dictionary containing list of values, e.g.
             {"train_loss": [...], "train_acc": [...],
-             "test_loss": [...], "test_acc": [...]}
+             "val_loss": [...], "val_acc": [...]}
     """
     Train_Loss = training_results['train_loss']
-    Test_Loss = training_results['test_loss']
+    Test_Loss = training_results['val_loss']
     epochs = range(len(training_results['train_loss']))
-    plt.figure(figsize=(18, 12))
+    plt.figure(figsize=(14, 8))
     plt.plot(epochs, Train_Loss, label='Train_Loss', linewidth=2)
-    plt.plot(epochs, Test_Loss, label='Val_Loss', linewidth=2)
+    plt.plot(epochs, Test_Loss, label='Validation_Loss', linewidth=2)
     plt.title("Loss Curves", fontsize=25)
     plt.xlabel("Epochs", fontsize=20)
-    plt.gca().xaxis.set_major_locator(MultipleLocator(4))
+    plt.gca().xaxis.set_major_locator(MultipleLocator(2))
+    plt.gca().yaxis.set_major_locator(MultipleLocator(0.25))
     plt.xticks(fontsize=18)
     plt.yticks(fontsize=18)
     plt.legend(fontsize=22)
@@ -159,14 +163,15 @@ def plot_loss_curves(training_results: dict[str, list[float]]):
 def plot_acc_curves(training_results: dict[str, list[float]]):
     
     Train_Accuracy = training_results['train_acc']
-    Test_Accuracy = training_results['test_acc']
+    Test_Accuracy = training_results['val_acc']
     epochs = range(len(training_results['train_loss']))
-    plt.figure(figsize=(18, 12))
+    plt.figure(figsize=(14, 18))
     plt.plot(epochs, Train_Accuracy, label='Train_Acc', linewidth=2)
-    plt.plot(epochs, Test_Accuracy, label='Val_Acc', linewidth=2)
+    plt.plot(epochs, Test_Accuracy, label='Validation_Acc', linewidth=2)
     plt.title("Acc Curves", fontsize=25)
     plt.xlabel("Epochs", fontsize=20)
-    plt.gca().xaxis.set_major_locator(MultipleLocator(4))
+    plt.gca().xaxis.set_major_locator(MultipleLocator(2))
+    plt.gca().yaxis.set_major_locator(MultipleLocator(0.05))
     plt.xticks(fontsize=18)
     plt.yticks(fontsize=18)
     plt.legend(fontsize=22)
@@ -174,16 +179,48 @@ def plot_acc_curves(training_results: dict[str, list[float]]):
     plt.show()
 
 
-
-
 def save_model(model, target_dir, model_name):
     
     target_dir_path = Path(target_dir)
     target_dir_path.mkdir(parents=True, exist_ok=True)
-    
+
     assert model_name.endswith(".pth") or model_name.endswith(".pt"), "model_name should end with '.pt' or '.pth'"
     model_save_path = target_dir_path / model_name
-
     print(f"[INFO] Saving model to: {model_save_path}")
     torch.save(obj=model.state_dict(), f=model_save_path)
 
+
+def eval_model(model, dataloader, device, num_classes, is_RNN):
+    test_acc = 0.0
+    test_precision = [0.0] * num_classes
+    test_recall = [0.0] * num_classes
+    model.eval()
+    with torch.no_grad():
+        for X_test, y_test in dataloader:
+            if is_RNN is True:
+                X_test, y_test = X_test.reshape(-1, 44, 64).to(device), y_test.to(device)
+            else:
+                X_test, y_test = X_test.to(device), y_test.to(device)
+            y_pred = model(X_test)
+            y_hat = y_pred.argmax(dim=1)
+            test_acc += torch.eq(y_hat, y_test).sum().item() / len(y_test)
+            for i in range(num_classes):
+                test_precision[i] += (sum((y_hat == i) & (y_test == i)) / sum(y_hat == i)).item()
+                test_recall[i] += (sum((y_hat == i) & (y_test == i)) / sum(y_test == i)).item()
+        test_acc /= len(dataloader)
+        precision = (sum(test_precision) / num_classes)/ len(dataloader)
+        recall = (sum(test_recall) / num_classes) / len(dataloader)
+        F1_Score = 2 * precision * recall / (precision + recall)
+        print("-" * 100)
+        print(f"model_name: {model.__class__.__name__} | "
+              f"model_acc: {test_acc:.4f} | "
+              f"model_precision: {precision:.4f} | "
+              f"model_recall: {recall:.4f} | "
+              f"model_F1_score: {F1_Score:.4f}")
+        
+        return {"model_name": model.__class__.__name__, 
+                "model_acc": test_acc, 
+                "model_precision": precision, 
+                "model_recall": recall, 
+                "model_test_F1": F1_Score}
+            